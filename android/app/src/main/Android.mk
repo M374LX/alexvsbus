b9af3abd67e374cd94228feefff7986823fbab5f
M374LX <wilsalx@gmail.com>
2024-11-20 15:28:27 -0300

    Ensure Android builds are reproducible by disabling NDK build-id

--- android/app/src/main/Android.mk
+++ android/app/src/main/Android.mk
@@ -11,18 +11,21 @@ LOCAL_SRC_FILES += $(addprefix $(PROJECT_ROOT)/raylib/,raudio.c rcore.c utils.c)
 
 LOCAL_C_INCLUDES := $(PROJECT_ROOT)/src $(PROJECT_ROOT)/raylib
 
-#We disable miniaudio's AAudio backend because we suspect it causes random
-#crashes when the game is closed
+# Disable miniaudio's AAudio backend because we suspect it causes random crashes
+# when the game is closed
 LOCAL_CFLAGS := -O2 -DGRAPHICS_API_OPENGL_ES2 -DPLATFORM_ANDROID -D__ANDROID__ \
 	-DMA_NO_AAUDIO -DMA_NO_RUNTIME_LINKING
-
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lOpenSLES -dl -lm -lc
 
 ifeq ($(TARGET_ARCH),arm)
 	LOCAL_CFLAGS += -mfloat-abi=softfp -mfpu=vfpv3-d16
 else ifeq ($(TARGET_ARCH),arm64)
 	LOCAL_CFLAGS += -mfix-cortex-a53-835769
 endif
+
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lOpenSLES -dl -lm -lc
+
+# Ensure Android builds are reproducible by disabling NDK build-id
+LOCAL_LDFLAGS := -Wl,--build-id=none
 
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
